a1ac5772f3676bef9976d004d87796aeebda3973
Maxwell Turpin <maxwell.turpin@tufts.edu>
2016-12-12 23:20:39 -0500

    working on writing to characteristics

--- basketball.js
+++ basketball.js
@@ -32,20 +32,29 @@ function connect() {
         })
         .then(service => {
             console.log('Getting Characteristics...');
-            ballNotify = service.getCharacteristic('0734594a-a8e7-4b1a-a6b1-cd5243059a57');
-            return service.getCharacteristic('8b00ace7-eb0b-49b0-bbe9-9aee0a26e1a3');
-        })
+            service.getCharacteristic('8b00ace7-eb0b-49b0-bbe9-9aee0a26e1a3')
+            .then(characteristic => {
+                ballWrite = characteristic
+            )
+            service.getCharacteristic('0734594a-a8e7-4b1a-a6b1-cd5243059a57')
+            .then(characteristic => {
+                ballNotify = characteristic
+            )
+            console.log('All ready!');
+            onConnected();
+            //return service.getCharacteristic('8b00ace7-eb0b-49b0-bbe9-9aee0a26e1a3');
+        })/*
         .then(characteristic => {
-            /*characteristics.forEach(characteristic => {
+            characteristics.forEach(characteristic => {
                 console.log('>> Characteristic: ' + characteristic.uuid + ' ' +
                     getSupportedProperties(characteristic));
-            });*/
+            });
             ballWrite = characteristic
             console.log('All ready!');
             onConnected();
             // Characteristic: 8b00ace7-eb0b-49b0-bbe9-9aee0a26e1a3 [WRITEWITHOUTRESPONSE]
             // Characteristic: 0734594a-a8e7-4b1a-a6b1-cd5243059a57 [NOTIFY]
-        })
+        })*/
         .catch(error => {
             console.log('Argh! ' + error);
         });
